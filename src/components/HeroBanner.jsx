ab3d885d9219a686c18614c09ebf8ab2ec74fa2c
Dgbusiness <dgbusiness26@gmail.com>
2022-10-03 15:17:10 -0400

    Starting my portfolio

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box } from "@mui/material";
+
+const HeroBanner = () => (
+    <Box 
+        className="heroBanner container" 
+        sx={{
+            height: { xs: "100vh", sm: "110vh", md: "100vh" },
+            bgcolor: "gray",
+            color: 'white',
+            display: 'flex',
+
+        }}
+    >
+        testing 
+    </Box>
+)
+
+export default HeroBanner
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HeroBanner from '../components/HeroBanner'
+
+const HomeLayout = () => (
+    <HeroBanner />
+)
+
+export default HomeLayout
