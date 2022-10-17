7cf5328e23ea6e3e5cbb3cbc61cff3f5a7d3e100
Dgbusiness <dgbusiness26@gmail.com>
2022-10-17 15:59:47 -0400

    Formik added but still not working properly

--- src/layouts/ContactLayout.jsx
+++ src/layouts/ContactLayout.jsx
@@ -1,57 +1,69 @@
-import {useState} from 'react';
-import { Box, Typography, Card, CardMedia, CardContent, CardActions, Button, Divider, FormControl, TextField } from "@mui/material";
+import { Box,  Button, TextField } from "@mui/material";
 import Title from '../components/Title.jsx'
 import { useFormik } from 'formik';
 import { withStyles } from '@mui/styles';
-import * as Yup from 'yup';
+import * as yup from 'yup';
 
-const ContactLayout = () => {
-
-    const StyledTextField = withStyles({
-        root: {
-            '& label.Mui-focused': {
-                color: 'white',
+const StyledTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#a80c06',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'white',
             },
-            '& .MuiInput-underline:after': {
-                borderBottomColor: '#a80c06',
+            '&:hover fieldset': {
+                borderColor: 'white',
             },
-            '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                    borderColor: 'white',
-                },
-                '&:hover fieldset': {
-                    borderColor: 'white',
-                },
-                '&.Mui-focused fieldset': {
-                    borderColor: '#a80c06',
-                },
+            '&.Mui-focused fieldset': {
+                borderColor: '#a80c06',
             },
         },
-    })(TextField);
+    },
+})(TextField);
 
-    /*Initial values (Formik)*/
-    const initialValues = {
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-    }
+/*Initial values (Formik)*/
+const initialValues = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+}
 
-    /*Validation schema with Yup (Formik)*/
-    const validationSchema = Yup.object({
-        firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-        lastName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-        email: Yup.string().email('Invalid email address').required('Required'),
-    })
+/*Validation schema with Yup (Formik)*/
+const validationSchema = yup.object({
+    name: yup
+    .string()
+    .required('Required')
+    .min(3, 'Name must be greater than 3 characters.')
+    .max(15, 'Name must be lower than 15 characters.'),
+    email: yup
+    .string()
+    .email('Invalid email address.')
+    .required('Required'),
+    subject: yup
+    .string()
+    .required('Required')
+    .min(5, 'Subject must be greater than 5 characters.')
+    .max(50, 'Subject must be lower than 50 characters.'),
+    message: yup
+    .string()
+    .required('Required')
+    .min(10, 'Message must be greater than 10 characters.')
+    .max(1000, 'Message must be lower than 1000 characters.'),
+})
 
-    /*onSubmit function (Formik)*/
-    const onSubmit = values => {
-        alert(JSON.stringify(values, null, 2));
-    } 
+/*onSubmit function (Formik)*/
+const onSubmit = values => {
+    console.log('entro')
+    alert(JSON.stringify(values, null, 2));
+} 
+
+const ContactLayout = () => {
 
     const formik = useFormik({
         initialValues,
@@ -78,48 +90,51 @@ const ContactLayout = () => {
             <form onSubmit={formik.handleSubmit}>
                 <StyledTextField
                     required
+                    id='name'
+                    name='name'
                     label="Name"
                     placeholder="Your name"
                     size='small'
                     margin='normal'
+                    value={formik.values.name}
                     onChange={formik.handleChange}
                     sx={{
                         width:'49%',
                         mr: '2%',
                     }}
-                    InputLabelProps={{ shrink: true }}
                 />
-                {formik.errors.name && formik.touched.name && formik.errors.name}
                 <StyledTextField
                     required
+                    name='email'
                     label="Email"
+                    type='email'
                     placeholder="Your email"
                     size='small'
                     margin='normal'
                     onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
                     value={formik.values.email}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
                     sx={{
                         width:'49%',
                     }}
-                    InputLabelProps={{ shrink: true }}
                 />
-                {formik.errors.email && formik.touched.email && formik.errors.email}
                 <StyledTextField
                     required
+                    name='subject'
                     label="Subject"
                     placeholder="Write a subject"
                     size='small'
                     margin='normal'
                     fullWidth
                     onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
                     value={formik.values.subject}
-                    InputLabelProps={{ shrink: true }}
+                    error={formik.touched.subject && Boolean(formik.errors.subject)}
+                    helperText={formik.touched.subject && formik.errors.subject}
                 />
-                {formik.errors.subject && formik.touched.subject && formik.errors.subject}
                 <StyledTextField
                     required
+                    name='message'
                     label="Message"
                     placeholder="Your message here"
                     fullWidth
@@ -127,12 +142,11 @@ const ContactLayout = () => {
                     rows={5}
                     margin='normal'
                     onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
                     value={formik.values.message}
-                    InputLabelProps={{ shrink: true }}
+                    error={formik.touched.message && Boolean(formik.errors.message)}
+                    helperText={formik.touched.message && formik.errors.message}
                 />
-                {formik.errors.message && formik.touched.message && formik.errors.message}
-                <Button type="submit">
+                <Button fullWidth type='submit' color='secondary' variant='contained'>
                     Submit
                 </Button>
             </form>
