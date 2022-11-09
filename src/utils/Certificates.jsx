8356911b70aa274dc5765e0d6cb1c62632e6bd51
Dgbusiness <dgbusiness26@gmail.com>
2022-11-09 15:27:05 -0400

    Laravel certificate

--- src/utils/Certificates.jsx
+++ src/utils/Certificates.jsx
@@ -1,12 +1,3 @@
 export const certificates = [
-
+    {name: 'PHP with Laravel for beginners - Become a Master in Laravel', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-5a6ed7cf-4542-4b8d-8ac6-f0c8e9f7dee5.jpg?v=1668021529000', url: 'https://www.udemy.com/certificate/UC-5a6ed7cf-4542-4b8d-8ac6-f0c8e9f7dee5/'},
 ]
-
-//export const certificates = [
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-    //{name: 'MERN eCommerce From Scratch', platform: 'udemy', img: 'https://udemy-certificate.s3.amazonaws.com/image/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6.jpg?v=1614522421000', url: 'https://ude.my/UC-10494a72-dd6e-49f2-8b35-b069fa31aae6'},
-//]
